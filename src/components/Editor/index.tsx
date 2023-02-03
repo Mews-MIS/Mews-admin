1310a4dfd9971d8b7a4fc885d1ccaa44a89894f1
Choi siun <siun0331@gmail.com>
2023-02-03 18:48:35 +0900

    feat: 에디터 작성 폼 제작 완료

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,18 +1,33 @@
 import { Editor } from "@toast-ui/react-editor";
-import "@toast-ui/editor/dist/toastui-editor.css";
 
-const Editor = () => {
+interface Props {
+  content?: string;
+  editorRef: React.MutableRefObject<any>;
+}
+const ContentEditor = ({ content = "", editorRef }: Props) => {
+  const toolbarItems = [
+    ["heading", "bold", "italic", "strike"],
+    ["hr"],
+    ["ul", "ol", "task"],
+    ["table", "link"],
+    ["image"],
+    ["code"],
+    ["scrollSync"],
+  ];
+
   return (
     <Editor
-      height={"500px"}
+      ref={editorRef}
+      initialValue={content || " "}
+      height={"100%"}
       previewStyle="vertical"
       initialEditType="wysiwyg"
       useCommandShortcut={false}
       hideModeSwitch={true}
+      toolbarItems={toolbarItems}
       language={"ko-KR"}
-      theme={"dark"}
     />
   );
 };
 
-export default Editor;
+export default ContentEditor;
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "@toast-ui/editor/dist/toastui-editor.css";
+import "@toast-ui/editor/dist/i18n/ko-kr";
 import { BrowserRouter } from "react-router-dom";
-import DefaultLayout from "./layouts/DefaultLayout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
