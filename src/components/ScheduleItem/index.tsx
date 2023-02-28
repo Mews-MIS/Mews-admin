858e5ec9c1d1556ee292206f933346aed48f04ec
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2023-02-28 14:18:54 +0900

    feat: admin-학사일정 레이아웃 작업

--- src/components/ScheduleItem/index.tsx
+++ src/components/ScheduleItem/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { IScheduleProps } from '../../pages/ScheduleEdit';
 import * as s from "./styles";
+import DeleteScheduleItem from "../../assets/Icon/DeleteScheduleItem.svg";
 
 const ScheduleItem = ({title, startDate, endDate}: IScheduleProps) => {
+  const startYear = startDate[0];
+  const startMonth = startDate[1];
+  const startDay = startDate[2];
+
+  const endYear = endDate[0];
+  const endMonth = endDate[1];
+  const endDay = endDate[2];
+  
   return (
     <s.Wrapper>
       <s.LeftSide>
-        <s.DeleteBtn>
-
-        </s.DeleteBtn>
-        <s.Title>
-          {title}
-        </s.Title>
+        <s.DeleteBtn src={DeleteScheduleItem} alt="삭제버튼" />
+        <s.Title>{title}</s.Title>
       </s.LeftSide>
-      <s.RightSide>{startDate} ~ {endDate}</s.RightSide>
+
+      <s.RightSide>{startYear}.{startMonth}.{startDay} - {endYear}.{endMonth}.{endDay}</s.RightSide>
     </s.Wrapper>
   )
 };
--- src/pages/ScheduleEdit.tsx
+++ src/pages/ScheduleEdit.tsx
@@ -23,10 +23,12 @@ const ScheduleEdit = () => {
 
   async function getData() {
     return await axios.get(
-      '서버url/calendar/getall',
+      'http://13.209.163.188:8080/calendar/getall',
       {
         headers: {
-          Authorization: `토큰값`
+          Authorization: `
+            Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0dGVzdDMxOUBuYXZlci5jb20iLCJhdXRoIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjc2OTY2NDYzLCJleHAiOjE2NzY5NzAwNjN9.4tcQkHsPiIlQcOzqtBVmSKAQceqELNxf-jlOznf0qehD7VgGOreh7M3p1ILmkB0OZS7z3C7oJA4AIvnnoi0lKw
+          `
         }
       }
     )
@@ -47,15 +49,68 @@ const ScheduleEdit = () => {
 
   return (
     <Wrapper>
-      {
-        data.map((item: IDataType) => {
-          return <ScheduleItem title={item.title} startDate={item.startDate} endDate={item.endDate} />
-        })
-      }
+      <Container>
+        <Title>현재 적용된 스케줄</Title>
+        <ScheduleListContainer>
+          {
+            data.map((item: IDataType) => {
+              return <ScheduleItem title={item.title} startDate={item.startDate} endDate={item.endDate} />
+            })
+          }
+        </ScheduleListContainer>
+      </Container>
+
+      <Container>
+        <Title>스케줄 추가</Title>
+          <ScheduleInput>
+            <ScheduleTitleInput type="text"/>
+            <ScheduleDateInput type="date" placeholder="시작 날짜"/>
+            <ScheduleDateInput type="date" placeholder="종료 날짜"/>
+          </ScheduleInput>
+      </Container>
+
+
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin-top: 50px;
+`
+
+const Title = styled.div`
+  font-size: large;
+  font-weight: bold;
+`;
+
+const ScheduleListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid;
+`;
+
+const ScheduleInput = styled.div`
+  display: flex;
+  width: 100%;
+  border: 1px solid;
+`;
+
+const ScheduleTitleInput = styled.input`
+  width: 60%;
+`;
+
+const ScheduleDateInput = styled.input`
+  width: 20%;
+`;
 
 export default ScheduleEdit;
