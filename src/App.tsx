2c8101a4bdf79711dbb2edfacfea903b730ad489
Choi siun <siun0331@gmail.com>
2023-03-08 16:23:08 +0900

    chore: eslint setting

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import CurationEdit from "./pages/CurationEdit";
 import EditorEdit from "./pages/EditorEdit";
 import ScheduleEdit from "./pages/ScheduleEdit";
+
 import NotFound from "./pages/NotFound";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Statics from "./pages/Statics";
@@ -16,21 +17,21 @@ function App() {
 
   const queryClient = new QueryClient();
   return (
-      <QueryClientProvider client={queryClient}>
-    <Routes>
-      <Route element={<DefaultLayout />}>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/article/new"} element={<ArticleWrite />} />
-        <Route path={"/article/edit"} element={<ArticleEditList />} />
-        <Route path={"/article/edit/:id"} element={<ArticleEditDetail />} />
-        <Route path={"/curation"} element={<CurationEdit />} />
-        <Route path={"/schedule"} element={<ScheduleEdit />} />
-        <Route path={"/editor"} element={<EditorEdit />} />
-        <Route path={"/statics"} element={<Statics />} />
-        <Route path={"*"} element={<NotFound />} />
-      </Route>
-    </Routes>
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/article/new"} element={<ArticleWrite />} />
+          <Route path={"/article/edit"} element={<ArticleEditList />} />
+          <Route path={"/article/edit/:id"} element={<ArticleEditDetail />} />
+          <Route path={"/curation"} element={<CurationEdit />} />
+          <Route path={"/schedule"} element={<ScheduleEdit />} />
+          <Route path={"/editor"} element={<EditorEdit />} />
+          <Route path={"/statics"} element={<Statics />} />
+          <Route path={"*"} element={<NotFound />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
